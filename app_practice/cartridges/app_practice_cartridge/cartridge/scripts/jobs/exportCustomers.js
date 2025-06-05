dbe51f452b7a8f5480b4159a67a16f095ed15c30
stefanjelkic00 <stefanjelkic2@gmail.com>
2025-06-05 16:57:05 +0200

    zzz

--- app_practice/cartridges/app_practice_cartridge/cartridge/scripts/jobs/exportCustomers.js
+++ app_practice/cartridges/app_practice_cartridge/cartridge/scripts/jobs/exportCustomers.js
@@ -29,7 +29,9 @@ function execute(parameters, stepExecution) {
 }
 
 function exportCustomersStreaming(xmlFilePath) {
-    const { xmlWriter, fileWriter } = FileSystemHelper.createXMLWriter(xmlFilePath);
+    const result = FileSystemHelper.createXMLWriter(xmlFilePath);
+    const xmlWriter = result.xmlWriter;
+    const fileWriter = result.fileWriter;
     const customerIterator = CustomerMgr.searchProfiles('custom.isExported != {0}', 'lastModified asc', [true]);
     let exportedCount = 0;
     
@@ -79,21 +81,47 @@ function writeCustomerXML(xmlWriter, profile) {
     FileSystemHelper.writeXMLElement(xmlWriter, 'newsletter-subscribed', (profile.custom.newsletterSubscribed || false).toString());
     FileSystemHelper.writeXMLElement(xmlWriter, 'newsletter-email', profile.custom.newsletterEmail || profile.email);
     
-    const addresses = profile.getAddressBook().getAddresses();
+    let addressBook = profile.getAddressBook();
+    let addresses = addressBook.getAddresses();
+    
+    
     if (addresses && addresses.length > 0) {
         xmlWriter.writeStartElement('addresses');
-        for (let i = 0; i < addresses.length; i++) {
-            const addr = addresses[i];
-            xmlWriter.writeStartElement('address');
-            xmlWriter.writeAttribute('id', addr.getID());
-            FileSystemHelper.writeXMLElement(xmlWriter, 'address1', addr.getAddress1() || '');
-            FileSystemHelper.writeXMLElement(xmlWriter, 'address2', addr.getAddress2() || '');
-            FileSystemHelper.writeXMLElement(xmlWriter, 'city', addr.getCity() || '');
-            FileSystemHelper.writeXMLElement(xmlWriter, 'house-nr', addr.custom.houseNr || '');
-            xmlWriter.writeEndElement();
+        
+        let addressArray = [];
+        let iterator = addresses.iterator();
+        while (iterator.hasNext()) {
+            addressArray.push(iterator.next());
         }
+        
+        for (let i = 0; i < addressArray.length; i++) {
+            let addr = addressArray[i];
+            Logger.info('Processing address ' + i + ' (ID: ' + addr.getID() + ') for customer ' + profile.customerNo);
+            writeAddressXML(xmlWriter, addr, addressBook, i);
+        }
+        
         xmlWriter.writeEndElement();
     }
+}
+
+function writeAddressXML(xmlWriter, addr, addressBook, index) {
+    xmlWriter.writeStartElement('address');
+    xmlWriter.writeAttribute('id', addr.getID());
+    xmlWriter.writeAttribute('index', index.toString());
+    
+    let preferredAddress = addressBook.getPreferredAddress();
+    let isPreferred = preferredAddress && (addr.getID() === preferredAddress.getID());
+    xmlWriter.writeAttribute('preferred', isPreferred.toString());
+    
+    FileSystemHelper.writeXMLElement(xmlWriter, 'address1', addr.getAddress1() || '');
+    FileSystemHelper.writeXMLElement(xmlWriter, 'address2', addr.getAddress2() || '');
+    FileSystemHelper.writeXMLElement(xmlWriter, 'city', addr.getCity() || '');
+    FileSystemHelper.writeXMLElement(xmlWriter, 'postal-code', addr.getPostalCode() || '');
+    FileSystemHelper.writeXMLElement(xmlWriter, 'state-code', addr.getStateCode() || '');
+    FileSystemHelper.writeXMLElement(xmlWriter, 'country-code', addr.getCountryCode() || '');
+    FileSystemHelper.writeXMLElement(xmlWriter, 'house-nr', addr.custom.houseNr || '');
+    
+    xmlWriter.writeEndElement();
 }
 
 exports.execute = execute;
