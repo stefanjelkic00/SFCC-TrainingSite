7730c6adaa6f1c236e09f5dafc10296020cdbde5
stefanjelkic00 <stefanjelkic2@gmail.com>
2025-06-16 15:17:57 +0200

    izmene na komentare..

--- app_practice/cartridges/app_practice_cartridge/cartridge/client/default/js/search/search.js
+++ app_practice/cartridges/app_practice_cartridge/cartridge/client/default/js/search/search.js
@@ -2,23 +2,41 @@
 
 const baseSearch = require('app_storefront_base/js/search/search');
 
-$(document).ready(function() {
-    baseSearch.filter();
-    baseSearch.closeRefinements();
-    baseSearch.resize();
-    baseSearch.sort();
-    baseSearch.showMore();
-    baseSearch.applyFilter(); 
-    baseSearch.showContentTab();
-    
+baseSearch.colorAttribute = function() {
     $('.container').on('click', '.color-attribute button', function (e) {
+        e.preventDefault();
         const seoUrl = $(this).data('seo-href');
         const searchUrl = $(this).data('href');
         
         if (seoUrl && seoUrl !== '#' && seoUrl !== searchUrl) {
-            window.history.pushState({}, null, seoUrl);
+            $.spinner().start();
+            $(this).trigger('search:filter', e);
+            
+            $.ajax({
+                url: searchUrl,
+                data: { 
+                    page: $('.grid-footer').data('page-number'),
+                    selectedUrl: searchUrl 
+                },
+                success: function (response) {
+                    $('.product-grid').html($(response).find('.product-grid').html());
+                    $('.refinements').html($(response).find('.refinements').html());
+                    window.history.pushState({}, null, seoUrl);
+                    $.spinner().stop();
+                },
+                error: function () {
+                    $.spinner().stop();
+                }
+            });
         }
     });
+};
+
+$(document).ready(function() {
+    ['filter', 'closeRefinements', 'resize', 'sort', 'showMore', 'applyFilter', 'showContentTab']
+        .forEach(method => baseSearch[method]());
+    
+    baseSearch.colorAttribute();
 });
 
 module.exports = baseSearch;
--- app_practice/cartridges/app_practice_cartridge/cartridge/models/search/attributeRefinementValue/base.js
+++ app_practice/cartridges/app_practice_cartridge/cartridge/models/search/attributeRefinementValue/base.js
@@ -1,29 +1,7 @@
 'use strict';
 
 const BaseAttributeValueCore = require('app_storefront_base/cartridge/models/search/attributeRefinementValue/base');
-const SEO_REFINE_ENDPOINT = 'Search-Show';
 
-function BaseAttributeValue(productSearch, refinementDefinition, refinementValue) {
-    this.productSearch = productSearch;
-    this.refinementDefinition = refinementDefinition;
-    this.refinementValue = refinementValue;
-    
-    this.initialize();
-}
+BaseAttributeValueCore.prototype.seoRefineEndpoint = 'Search-Show';
 
-BaseAttributeValue.prototype = Object.assign({}, BaseAttributeValueCore.prototype, {
-    initialize: function () {
-        this.id = this.refinementValue.ID;
-        this.presentationId = this.refinementValue.presentationID;
-        this.value = this.refinementValue.value;
-        this.hitCount = this.refinementValue.hitCount;
-        this.selectable = this.refinementValue.hitCount > 0;
-        this.actionEndpoint = BaseAttributeValueCore.actionEndpoint;
-        
-        this.seoRefineEndpoint = SEO_REFINE_ENDPOINT;
-    }
-});
-
-module.exports = BaseAttributeValue;
-module.exports.actionEndpoint = BaseAttributeValueCore.actionEndpoint;
-module.exports.seoRefineEndpoint = SEO_REFINE_ENDPOINT;
+module.exports = BaseAttributeValueCore;
