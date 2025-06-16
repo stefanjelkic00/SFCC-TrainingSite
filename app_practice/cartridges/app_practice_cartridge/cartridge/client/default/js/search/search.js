0c1b89917941404c20b2a3d0daae84a5bebc85cf
stefanjelkic00 <stefanjelkic2@gmail.com>
2025-06-16 16:46:19 +0200

    sss

--- app_practice/cartridges/app_practice_cartridge/cartridge/client/default/js/search/search.js
+++ app_practice/cartridges/app_practice_cartridge/cartridge/client/default/js/search/search.js
@@ -2,41 +2,68 @@
 
 const baseSearch = require('app_storefront_base/js/search/search');
 
-baseSearch.seoAttributeFilter = function() {
-    $('.container').on('click', '[data-seo-href]', function (e) {
-        const seoUrl = $(this).data('seo-href');
-        const searchUrl = $(this).data('href');
-        
-        if (seoUrl && seoUrl !== '#' && seoUrl !== searchUrl) {
-            e.preventDefault();
-            $.spinner().start();
-            $(this).trigger('search:filter', e);
-            
-            $.ajax({
-                url: searchUrl,
-                data: { 
-                    page: $('.grid-footer').data('page-number'),
-                    selectedUrl: searchUrl 
-                },
-                success: function (response) {
-                    $('.product-grid').html($(response).find('.product-grid').html());
-                    $('.refinements').html($(response).find('.refinements').html());
-                    window.history.pushState({}, null, seoUrl);
-                    $.spinner().stop();
-                },
-                error: function () {
-                    $.spinner().stop();
-                }
-            });
-        }
+function parseResults(response) {
+    var $results = $(response);
+    
+    [
+        '.grid-header',
+        '.header-bar',
+        '.header.page-title',
+        '.product-grid',
+        '.show-more',
+        '.filter-bar'
+    ].forEach(function (selector) {
+        var $updates = $results.find(selector);
+        $(selector).empty().html($updates.html());
     });
-};
+
+    $('.refinement.active').each(function () {
+        $(this).removeClass('active');
+        var activeDiv = $results.find('.' + $(this)[0].className.replace(/ /g, '.'));
+        activeDiv.addClass('active');
+        activeDiv.find('button.title').attr('aria-expanded', 'true');
+    });
+    
+    var $refinements = $results.find('.refinements');
+    $('.refinements').empty().html($refinements.html());
+}
+
+Object.assign(baseSearch, {
+    seoAttributeFilter: function() {
+        $('.container').on('click', '[data-seo-href]', function (e) {
+            const seoUrl = $(this).data('seo-href');
+            const searchUrl = $(this).data('href');
+            
+            if (seoUrl && seoUrl !== '#' && seoUrl !== searchUrl) {
+                e.preventDefault();
+                $.spinner().start();
+                
+                $.ajax({
+                    url: searchUrl,
+                    data: { 
+                        page: $('.grid-footer').data('page-number'),
+                        selectedUrl: searchUrl 
+                    },
+                    success: function (response) {
+                        parseResults(response);
+                        window.history.pushState({}, null, seoUrl);
+                        $.spinner().stop();
+                    },
+                    error: function () {
+                        $.spinner().stop();
+                    }
+                });
+            }
+        });
+    }
+});
 
 $(document).ready(function() {
-    ['filter', 'closeRefinements', 'resize', 'sort', 'showMore', 'applyFilter', 'showContentTab']
-        .forEach(method => baseSearch[method]());
-    
-    baseSearch.seoAttributeFilter();
+    Object.keys(baseSearch).forEach(function(methodName) {
+        if (typeof baseSearch[methodName] === 'function') {
+            baseSearch[methodName]();
+        }
+    });
 });
 
 module.exports = baseSearch;
