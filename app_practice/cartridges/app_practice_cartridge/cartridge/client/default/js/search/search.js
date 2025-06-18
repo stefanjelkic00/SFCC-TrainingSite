ab947705a9a1abaccddfea664776bb1df3e5dbdf
stefanjelkic00 <stefanjelkic2@gmail.com>
2025-06-18 15:48:13 +0200

    Uradjen Task10.... kroz 2 fajla...

--- app_practice/cartridges/app_practice_cartridge/cartridge/client/default/js/search/search.js
+++ app_practice/cartridges/app_practice_cartridge/cartridge/client/default/js/search/search.js
@@ -3,16 +3,9 @@
 const baseSearch = require('app_storefront_base/search/search');
 
 $(document).on('ajaxSuccess.searchUrlUpdate', function(event, xhr, settings) {
-    console.log('AJAX Success detected:', settings.url);
-    
     if (settings.url && (settings.url.indexOf('ShowAjax') > -1 || settings.url.indexOf('UpdateGrid') > -1)) {
-        console.log('URL matches - looking for permalink...');
-        
         const serverUrl = $(xhr.responseText).find('.permalink').val();
-        console.log('Found permalink:', serverUrl);
-        
         if (serverUrl) {
-            console.log('Updating browser URL to:', serverUrl);
             window.history.replaceState({}, null, serverUrl);
         }
     }
--- app_practice/cartridges/app_practice_cartridge/cartridge/client/default/js/util.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-module.exports = require('app_storefront_base/util');
