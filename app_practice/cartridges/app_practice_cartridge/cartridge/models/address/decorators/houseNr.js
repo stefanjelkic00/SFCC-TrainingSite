79d8088fecdfa00b46d848a2b6d0d00dc43dcde0
stefanjelkic00 <stefanjelkic2@gmail.com>
2025-07-04 15:05:27 +0200

    Task14 uradjen po prvim komentarima

--- app_practice/cartridges/app_practice_cartridge/cartridge/models/address/decorators/houseNr.js
+++ app_practice/cartridges/app_practice_cartridge/cartridge/models/address/decorators/houseNr.js
@@ -1,19 +1,14 @@
 'use strict';
 
-/**
- * Decorates the address object with the custom houseNr field using Object.defineProperty.
- * @param {Object} addressObj - The plain address model object to decorate.
- * @param {dw.customer.CustomerAddress} address - The real SFCC address object.
- */
 module.exports = function houseNr(addressObj, address) {
     if (!addressObj || !address) {
         return;
     }
+    
     Object.defineProperty(addressObj, 'houseNr', {
         enumerable: true,
-        value:
-            (address.custom && address.custom.houseNr) ||
-            (address.raw && address.raw.custom && address.raw.custom.houseNr) ||
-            ''
+        value: (address.custom && 'houseNr' in address.custom && address.custom.houseNr) || 
+               ('raw' in address && address.raw && address.raw.custom && 'houseNr' in address.raw.custom && address.raw.custom.houseNr) || 
+               ''
     });
-}; 
+};
