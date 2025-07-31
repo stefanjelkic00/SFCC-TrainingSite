e773eaf297c5ecc34f1ea8160dfc42aeaa6b2827
Stefan Jelkić <s.jelkic@beeit.rs>
2025-07-31 16:20:07 +0200

    Task22 prepravljen..

--- app_practice/cartridges/app_practice_cartridge/cartridge/client/default/js/checkout/billing.js
+++ app_practice/cartridges/app_practice_cartridge/cartridge/client/default/js/checkout/billing.js
@@ -1,11 +1,42 @@
 'use strict';
 
 const base = require('base/checkout/billing');
- 
+
+base.methods.updatePaymentInformation = function(order) {
+    const $paymentSummary = $('.payment-details');
+    let htmlToAppend = '';
+
+    if (order.billing.payment && order.billing.payment.selectedPaymentInstruments
+        && order.billing.payment.selectedPaymentInstruments.length > 0) {
+        
+        const instrument = order.billing.payment.selectedPaymentInstruments[0];
+        
+        if (instrument.paymentMethod === 'CASH_ON_DELIVERY') {
+            htmlToAppend = '<div class="cash-on-delivery-summary">' +
+                '<div class="payment-method-name">' +
+                '<span>Payment in cash.</span>' +
+                '</div>' +
+                '</div>';
+        } else {
+            htmlToAppend += '<span>' + order.resources.cardType + ' '
+                + instrument.type
+                + '</span><div>'
+                + instrument.maskedCreditCardNumber
+                + '</div><div><span>'
+                + order.resources.cardEnding + ' '
+                + instrument.expirationMonth
+                + '/' + instrument.expirationYear
+                + '</span></div>';
+        }
+    }
+
+    $paymentSummary.empty().append(htmlToAppend);
+};
+
 base.paymentTabs = function () {
     $('body').on('click', '.payment-options .nav-item', function (e) {
         e.preventDefault();
-        var methodID = $(this).data('method-id');
+        const methodID = $(this).data('method-id');
         $('.payment-information').data('payment-method-id', methodID);
         
         $('.payment-options .nav-link').removeClass('active');
@@ -17,10 +48,25 @@ base.paymentTabs = function () {
             $('#credit-card-content').removeClass('active show');
             $('#cash-on-delivery-content').addClass('active show');
             $('input[name$="_paymentMethod"]').val('CASH_ON_DELIVERY');
+            
+            $('.payment-details').html(
+                '<div class="cash-on-delivery-summary">' +
+                '<div class="payment-method-name">' +
+                '<span>Payment in cash.</span>' +
+                '</div>' +
+                '</div>'
+            );
+            
         } else if (methodID === 'CREDIT_CARD') {
             $('#credit-card-content').addClass('active show');
             $('#cash-on-delivery-content').removeClass('active show');
             $('input[name$="_paymentMethod"]').val('CREDIT_CARD');
+            
+            $('.payment-details').html(
+                '<span>Credit undefined</span><br>' +
+                '<span>undefined</span><br>' +
+                '<span>Ending undefined/undefined</span>'
+            );
         }
     });
 };
