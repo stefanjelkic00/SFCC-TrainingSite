c30125bd85f043a3b0ad017f77d0ade9b196c27f
Stefan Jelkić <s.jelkic@beeit.rs>
2025-08-04 14:46:58 +0200

    Task24. ispravka3

--- app_practice/cartridges/app_practice_cartridge/cartridge/scripts/hooks/order/orderUpdateHook.js
+++ app_practice/cartridges/app_practice_cartridge/cartridge/scripts/hooks/order/orderUpdateHook.js
@@ -6,14 +6,17 @@ const Transaction = require('dw/system/Transaction');
 exports.afterPATCH = function(order, orderInput) {
     Transaction.wrap(function() {
         for (let i = 0; i < orderInput.shipments.length; i++) {
-            for (let j = 0; j < order.shipments.length; j++) {
-                if (orderInput.shipments[i].shipment_id === order.shipments[j].ID && 
-                    orderInput.shipments[i].c_trackingNumber) {
-                    order.shipments[j].custom.trackingNumber = orderInput.shipments[i].c_trackingNumber;
-                }
+            if (orderInput.shipments[i].c_trackingNumber) {
+                let shipment = order.getShipment(orderInput.shipments[i].shipment_id);
+                shipment.custom.trackingNumber = orderInput.shipments[i].c_trackingNumber;
             }
         }
     });
     
     return new Status(Status.OK);
 };
+// getShipmentNo() : String
+
+// Returns the shipment number for this shipment.  jel mozda ovo ?  getID() : String
+
+// Returns the ID of this shipment ("me" for the default shipment). ili ovo ? 
