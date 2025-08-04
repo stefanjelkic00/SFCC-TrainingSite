f8b183cffcc5bb9cb4855da64c16763efc820604
Stefan Jelkić <s.jelkic@beeit.rs>
2025-08-04 14:50:45 +0200

    asd

--- app_practice/cartridges/app_practice_cartridge/cartridge/scripts/hooks/order/orderUpdateHook.js
+++ app_practice/cartridges/app_practice_cartridge/cartridge/scripts/hooks/order/orderUpdateHook.js
@@ -8,15 +8,12 @@ exports.afterPATCH = function(order, orderInput) {
         for (let i = 0; i < orderInput.shipments.length; i++) {
             if (orderInput.shipments[i].c_trackingNumber) {
                 let shipment = order.getShipment(orderInput.shipments[i].shipment_id);
-                shipment.custom.trackingNumber = orderInput.shipments[i].c_trackingNumber;
+                if (shipment) {
+                    shipment.custom.trackingNumber = orderInput.shipments[i].c_trackingNumber;
+                }
             }
         }
     });
     
     return new Status(Status.OK);
 };
-// getShipmentNo() : String
-
-// Returns the shipment number for this shipment.  jel mozda ovo ?  getID() : String
-
-// Returns the ID of this shipment ("me" for the default shipment). ili ovo ? 
